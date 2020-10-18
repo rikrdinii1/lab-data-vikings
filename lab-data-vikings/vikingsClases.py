e0f140ed0c1b273b0a0ec016f5dd55effb733803
rikrdinii <rikrdinii@hotmail.com>
2020-10-17 22:46:21 -0500

    Vikingos y Sajones Listo

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,24 +1,47 @@
 
 # Soldier
-
-
 class Soldier:
-    pass
+
+	pass
 
 # Viking
-
-
 class Viking:
-    pass
+	
+	def __init__(self,name, health,strength):
+		self.name = name
+		self.health = health
+		self.strength = strength
 
+	def attack(self):
+		return self.strength
+		
+	def receiveDamage(self,health):
+		if self.health == health:
+			return str(self.name + ' has died in act of combat')
+		elif health - self.health:
+			self.health = self.health - health
+			return str(self.name +' has received '+str(health)+ ' points of damage')
+	
+	def battleCry(self):
+		return str('Odin Owns You All!')	
+		
 # Saxon
-
-
 class Saxon:
-    pass
+	
+	def __init__(self,health,strength):
+		self.health = health
+		self.strength = strength
+
+	def attack(self):
+		return self.strength
+		
+	def receiveDamage(self,health):
+		if self.health == health:
+			return str('A Saxon has died in combat')
+		elif health - self.health:
+			self.health = self.health - health
+			return str('A Saxon has received '+str(health)+' points of damage') 
 
 # War
-
-
 class War:
     pass
